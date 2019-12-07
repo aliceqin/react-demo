88e58bbb97a06b8cc3a3bcddf79064970491b4dd
marick <marick@jhosdeMacBook-Pro.local>
2019-12-07 23:14:28 +0800

    confirm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import './App.css';
-import InputNumber from './InputNumber/InputNumber'
+import InputNumber from './InputNumber/inputNumber'
 
 function App(){
   const [value,setValue] = useState('aaa')
---
+++ src/Confirm/confirm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Confirm extends React.Component{
+   
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default Confirm
